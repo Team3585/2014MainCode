eab27acfd3d2ab85e55fecb3e9ec6f3d88eb675a
Andeerew <andrewfriend@yahoo.com>
2014-02-01 11:29:43 -0500

    Updated ForkLift Subsystem!

---
+++ src/edu/wpi/first/wpilibj/templates/commands/GoalieUp.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.commands;
+
+import edu.wpi.first.wpilibj.templates.OI;
+import edu.wpi.first.wpilibj.templates.subsystems.Goalie;
+
+/**
+ *
+ * @author Developer
+ */
+public class GoalieUp extends CommandBase {
+    
+    public GoalieUp() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        Goalie.goalDownLimitSwitch.get();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return (OI.goalie.atGoalieUpStop());
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+        OI.goalie.GoalieStop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
