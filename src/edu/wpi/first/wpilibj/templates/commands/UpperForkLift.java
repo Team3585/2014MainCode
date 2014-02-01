e0e616bbc7c6dcb751035acf795ac2c40743722f
Team3585 <roguerobotsof4h@gmail.com>
2014-02-01 11:55:51 -0500

    Finalized Dumper commands and subsystem.

--- src/edu/wpi/first/wpilibj/templates/commands/UpperForkLift.java
+++ src/edu/wpi/first/wpilibj/templates/commands/UpperForkLift.java
@@ -29,7 +29,7 @@ public class UpperForkLift extends CommandBase {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return (OI.forkLift.atUpperStop());
+        return (OI.forkLift.atUpperStop() || !OI.raiseForkLiftButton.get());
     }
 
     // Called once after isFinished returns true
