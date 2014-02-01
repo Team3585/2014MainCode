611c9b2b7eda2cd5f79d1370f59ff65fec0813e0
Team3585 <roguerobotsof4h@gmail.com>
2014-02-01 11:38:46 -0500

    Finalized Dumper commands and subsystem.

--- src/edu/wpi/first/wpilibj/templates/OI.java
+++ src/edu/wpi/first/wpilibj/templates/OI.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj.templates.subsystems.Dumper;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.templates.subsystems.Goalie;
+import edu.wpi.first.wpilibj.templates.commands.UpperForkLift;
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
@@ -32,6 +33,10 @@ public class OI {
         }
         
         return driveStick;
+ 
+    }
+    public OI(){
+   
         raiseForkLiftButton.whenPressed(new UpperForkLift());
     }
     
